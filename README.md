a68d8be43e2021c1721f7e6656b5f534e0a5dad2
hkab <khoahockithuatmc@gmail.com>
2021-05-10 10:29:23 +0700

    change project structure, add map to detail post

--- README.md
+++ README.md
@@ -1,16 +1,15 @@
-# data_warehouse_app
+# wemapgl
 
-A new Flutter application.
+SDK Bản đồ WeMap trên Flutter
 
-## Getting Started
+## IOS Configuration
 
-This project is a starting point for a Flutter application.
+1. Nhận API Key từ nhóm phát triển
 
-A few resources to get you started if this is your first Flutter project:
+2. Thêm API Key vào file main.dart
 
-- [Lab: Write your first Flutter app](https://flutter.dev/docs/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://flutter.dev/docs/cookbook)
+```
 
-For help getting started with Flutter, view our
-[online documentation](https://flutter.dev/docs), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
+  WEMAP.Configuration.setWeMapKey('YOUR_API_KEY');
+
+```
---
+++ data_warehouse_app/lib/main.dart
@@ -0,0 +1,40 @@
+import 'dart:io';
+import 'package:data_warehouse_app/app_theme.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'navigation_home_screen.dart';
+import 'package:wemapgl/wemapgl.dart' as WEMAP;
+
+void main() {
+  WEMAP.Configuration.setWeMapKey('GqfwrZUEfxbwbnQUhtBMFivEysYIxelQ');
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter UI',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        textTheme: AppTheme.textTheme,
+        platform: TargetPlatform.iOS,
+      ),
+      home: NavigationHomeScreen(),
+    );
+  }
+}
+
+class HexColor extends Color {
+  HexColor(final String hexColor) : super(_getColorFromHex(hexColor));
+
+  static int _getColorFromHex(String hexColor) {
+    hexColor = hexColor.toUpperCase().replaceAll('#', '');
+    if (hexColor.length == 6) {
+      hexColor = 'FF' + hexColor;
+    }
+    return int.parse(hexColor, radix: 16);
+  }
+}
